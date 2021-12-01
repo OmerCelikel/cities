da3912b5b7b6035b8f5b69f55afaf7022a69b33c
Ömer <omeroguzcelikel@gmail.com>
2021-12-01 17:16:45 +0300

    Initial Commit

---
+++ cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cities
+//
+//  Created by Ömer Oğuz Çelikel on 1.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
